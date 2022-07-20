830894790c8b85669daff94611774be2965774c8
Mario98casta <109645043+Mario98casta@users.noreply.github.com>
2022-07-20 09:05:23 -0600

    Add files via upload

---
+++ hola mundo/src/hola.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class hola {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+
+        System.out.println("ingrese su nombre:");
+        String nombre =entrada. nextLine();
+        System.out.printf("bienvenido "+nombre+ " a progra 2022 ");
+    }
+}
